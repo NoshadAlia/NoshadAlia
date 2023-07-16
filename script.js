c25b4b2696315f44afc1281eeac332360cb7bbe7
NoshadAli <121970724+NoshadAlia@users.noreply.github.com>
2023-07-16 12:47:45 +0500

    JavaScript
    This Project is Calculator Operators (+ , - , * , / )  Results in console.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getANumber(){
     let number=prompt   ("Enter a number")
     let result;
-    for (let i =1; i <=20; i++) {
+    for (let i =1; i <=10; i++) {
         result = number * i ;
         console.log(
             number + "x" + i + "=" + result
